ed0b27c10288d9981973fd710fdac3da3da72b3d
Noel Pascal Schwabenland <noel@schwabenland.ch>
2019-02-07 17:19:20 +0100

    Test2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 9999
+const express = require('express');
+const app = express();
+const port = 80;
 
-app.get('/', (req, res) => res.send('Hello GitWorld!'))
+app.get('/', (req, res) => res.send('Hello GitWorld!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
